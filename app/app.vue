8131f6841168764a27bbb4c3450169ec9a7109af
barik784 <ahammad.barik@enosisbd.com>
2025-12-16 19:37:15 +0600

    Added nuxt icon module.

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">View Map <Icon name="tabler:map"/></button>
-   
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ['./app/assets/css/main.css'],
+  css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
